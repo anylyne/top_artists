58b8e2de94df22589b69ab47aa6d22f8b0874849
Carol Mascarenhas <anylyne@gmail.com>
2021-03-16 00:27:57 +1100

    adding rubocop
    fixing: TrailingWhitespace, FrozenStringLiteralComment, IndentationStyle

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,25 +1,26 @@
+# frozen_string_literal: true
 class ArtistsController < ApplicationController
-	def show
-		@name = params['name']
-		
-		client = LastFmFinder.new(params['page'])
-		@tracks = client.get_top_tracks(@name)
+  def show
+    @name = params['name']
 
-		prepare_render(client)
-	end
+    client = LastFmFinder.new(params['page'])
+    @tracks = client.get_top_tracks(@name)
 
-	def index
-		@country = params['country']
-		
-		client = LastFmFinder.new(params['page'])
-		@artists = client.get_top_artists(@country)
+    prepare_render(client)
+  end
 
-		prepare_render(client)
-	end
+  def index
+    @country = params['country']
 
-	protected
-	def prepare_render(client)
-		@pagination = client.pagination
-		flash.now[:error] = client.error if !client.error.blank?	
-	end
+    client = LastFmFinder.new(params['page'])
+    @artists = client.get_top_artists(@country)
+
+    prepare_render(client)
+  end
+
+  protected
+  def prepare_render(client)
+    @pagination = client.pagination
+    flash.now[:error] = client.error if !client.error.blank?
+  end
 end
